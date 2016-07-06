62adcbb391a205b3863e94b163725092722d283f
Caleb <caleb.kleveter@gmail.com>
2016-07-06 09:51:44 -0500

    Added NPFNetwork.

---
+++ Napalm/Classes/APIClient.swift
@@ -0,0 +1,103 @@
+//
+//  APIClient.swift
+//  Pods
+//
+//  Created by Caleb Kleveter on 7/6/16.
+//
+//
+
+import Foundation
+
+public let NPFNetworkingErrorDomain = "com.napalm.Napalm.NetworkingError"
+public let MissingHTTPResponseError: Int = 10
+public let UnexpectedResponseError: Int = 20
+
+typealias JSON = [String : AnyObject]
+typealias JSONTaskCompletion = (JSON?, HTTPURLResponse?, NSError?) -> Void
+typealias JSONTask = URLSessionDataTask
+
+enum APIResult<T> {
+    case success(T)
+    case failure(ErrorProtocol)
+}
+
+protocol JSONDecodable {
+    init?(JSON: [String : AnyObject])
+}
+
+protocol Endpoint {
+    var baseURL: URL { get }
+    var path: String { get }
+    var request: URLRequest { get }
+}
+
+protocol APIClient {
+    var configuration: URLSessionConfiguration { get }
+    var session: URLSession { get }
+    
+    func JSONTaskWithRequest(_ request: URLRequest, completion: JSONTaskCompletion) -> JSONTask
+    func fetch<T: JSONDecodable>(_ request: URLRequest, parse: (JSON) -> T?, completion: (APIResult<T>) -> Void)
+}
+
+extension APIClient {
+    func JSONTaskWithRequest(_ request: URLRequest, completion: JSONTaskCompletion) -> JSONTask {
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            
+            guard let HTTPResponse = response as? HTTPURLResponse else {
+                let userInfo = [
+                    NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "")
+                ]
+                
+                let error = NSError(domain: NPFNetworkingErrorDomain, code: MissingHTTPResponseError, userInfo: userInfo)
+                completion(nil, nil, error)
+                return
+            }
+            
+            if data == nil {
+                if let error = error {
+                    completion(nil, HTTPResponse, error)
+                }
+            } else {
+                switch HTTPResponse.statusCode {
+                case 200:
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String : AnyObject]
+                        completion(json, HTTPResponse, nil)
+                    } catch let error as NSError {
+                        completion(nil, HTTPResponse, error)
+                    }
+                default: print("Received HTTP Response: \(HTTPResponse.statusCode) - not handled")
+                }
+            }
+        }
+        
+        return task
+    }
+    
+    func fetch<T>(_ request: URLRequest, parse: (JSON) -> T?, completion: (APIResult<T>) -> Void) {
+        
+        let task = JSONTaskWithRequest(request) { json, response, error in
+            
+            DispatchQueue.main.async {
+                guard let json = json else {
+                    if let error = error {
+                        completion(.failure(error))
+                    } else {
+                        // TODO: Implement Error Handling
+                    }
+                    return
+                }
+                
+                if let value = parse(json) {
+                    completion(.success(value))
+                } else {
+                    let error = NSError(domain: NPFNetworkingErrorDomain, code: UnexpectedResponseError, userInfo: nil)
+                    completion(.failure(error))
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
---
+++ Napalm/Classes/Networking.swift
@@ -0,0 +1,52 @@
+//
+//  Networking.swift
+//  Pods
+//
+//  Created by Caleb Kleveter on 7/6/16.
+//
+//
+
+import Foundation
+
+public protocol Initable {
+    init?(JSON: [String : AnyObject])
+}
+
+public class NPFNetwork: APIClient {
+    
+    let configuration: URLSessionConfiguration
+    lazy var session: URLSession = {
+        return URLSession(configuration: self.configuration)
+    }()
+    
+    public let url: String
+    
+    init(config: URLSessionConfiguration, URL: String) {
+        self.configuration = config
+        self.url = URL
+    }
+    
+    convenience init(URL: String) {
+        self.init(config: URLSessionConfiguration.default(), URL: URL)
+    }
+    
+    var request: URLRequest {
+        let url = URL(string: self.url)
+        return URLRequest(url: url!)
+    }
+    
+    
+    func fetchData<T: Initable>(withCompletion completion: (APIResult<T>) -> Void) {
+        
+        fetch(request, parse: { json -> T? in
+            // Parse from JSON response to CurrentWeather
+            
+            if let currentWeatherDictionary = json["currently"] as? [String : AnyObject] {
+                return T(JSON: currentWeatherDictionary)
+            } else {
+                return nil
+            }
+            
+            }, completion: completion)
+    }
+}
